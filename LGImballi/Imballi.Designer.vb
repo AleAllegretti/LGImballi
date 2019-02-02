1f87ec8df9a1ea9a9011d27f692251a533909005
AleAllegretti <alessandro.allegretti1@gmail.com>
2019-02-02 11:20:12 +0100

    Add project files.

---
+++ LGImballi/Imballi.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Imballi
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Alt_B = New System.Windows.Forms.TextBox()
+        Me.Larg = New System.Windows.Forms.TextBox()
+        Me.Lung = New System.Windows.Forms.TextBox()
+        Me.Alt_I = New System.Windows.Forms.TextBox()
+        Me.Larg_I = New System.Windows.Forms.TextBox()
+        Me.Lungh_I = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Mat = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Alt_B
+        '
+        Me.Alt_B.Location = New System.Drawing.Point(94, 42)
+        Me.Alt_B.Name = "Alt_B"
+        Me.Alt_B.Size = New System.Drawing.Size(100, 20)
+        Me.Alt_B.TabIndex = 0
+        '
+        'Larg
+        '
+        Me.Larg.Location = New System.Drawing.Point(94, 88)
+        Me.Larg.Name = "Larg"
+        Me.Larg.Size = New System.Drawing.Size(100, 20)
+        Me.Larg.TabIndex = 1
+        '
+        'Lung
+        '
+        Me.Lung.Location = New System.Drawing.Point(94, 143)
+        Me.Lung.Name = "Lung"
+        Me.Lung.Size = New System.Drawing.Size(100, 20)
+        Me.Lung.TabIndex = 2
+        '
+        'Alt_I
+        '
+        Me.Alt_I.Location = New System.Drawing.Point(255, 42)
+        Me.Alt_I.Name = "Alt_I"
+        Me.Alt_I.Size = New System.Drawing.Size(100, 20)
+        Me.Alt_I.TabIndex = 3
+        '
+        'Larg_I
+        '
+        Me.Larg_I.Location = New System.Drawing.Point(255, 88)
+        Me.Larg_I.Name = "Larg_I"
+        Me.Larg_I.Size = New System.Drawing.Size(100, 20)
+        Me.Larg_I.TabIndex = 4
+        '
+        'Lungh_I
+        '
+        Me.Lungh_I.Location = New System.Drawing.Point(255, 143)
+        Me.Lungh_I.Name = "Lungh_I"
+        Me.Lungh_I.Size = New System.Drawing.Size(100, 20)
+        Me.Lungh_I.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(94, 194)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 32)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Calcola"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Mat
+        '
+        Me.Mat.Location = New System.Drawing.Point(255, 194)
+        Me.Mat.Name = "Mat"
+        Me.Mat.Size = New System.Drawing.Size(100, 20)
+        Me.Mat.TabIndex = 8
+        '
+        'Imballi
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(460, 264)
+        Me.Controls.Add(Me.Mat)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Lungh_I)
+        Me.Controls.Add(Me.Larg_I)
+        Me.Controls.Add(Me.Alt_I)
+        Me.Controls.Add(Me.Lung)
+        Me.Controls.Add(Me.Larg)
+        Me.Controls.Add(Me.Alt_B)
+        Me.Name = "Imballi"
+        Me.Text = "Imballi"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Alt_B As TextBox
+    Friend WithEvents Larg As TextBox
+    Friend WithEvents Lung As TextBox
+    Friend WithEvents Alt_I As TextBox
+    Friend WithEvents Larg_I As TextBox
+    Friend WithEvents Lungh_I As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Mat As TextBox
+End Class
---
+++ LGImballi/Imballi.vb
@@ -0,0 +1,106 @@
+﻿Public Class Imballi
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Alt_b, Lung_N, Larg_N, Alt_I, Lung_I, Larg_I, Vol_N, Vol_Imb, Somma_circ, i, ang, Alt_N, r As Decimal
+        Dim Num_giri, Larg_cassa, Larg_cassa1, Alt_cassa, Costo, Lato_1, Lato_2, Lato_3, Larg_b, nstrati, n As Decimal
+        Dim Materiale As String
+
+        'Variable assignment
+        Materiale = "Ferro"
+        Alt_b = Me.Alt_B.Text
+        Lung_N = Me.Lung.Text
+        Larg_N = Me.Larg.Text
+        Vol_N = Lung_N * Alt_b
+        Vol_Imb = -0.0276 * Vol_N ^ 2 + 1.8935 * Vol_N - 0.247
+
+        'Wooden pallet
+        Somma_circ = 0
+        i = 0
+        ang = 0
+        If Vol_N < 3.1 And Lung_N < 25 Then
+            Materiale = "Legno"
+            Alt_N = Alt_b + 0.002
+            r = Alt_N + 0.02
+            Num_giri = 0
+            Do Until Somma_circ > Lung_N
+                If i = 40 Then
+                    r = r + Alt_N + 0.002
+                    i = 0
+                End If
+                ang = ang + 9
+                Somma_circ = (2 * 3.14 * r) / 40 + Somma_circ
+                Num_giri = Num_giri + 0.025
+                i = i + 1
+            Loop
+            Larg_cassa = r * 2 + 0.2
+
+            If Larg_cassa < 0.8 Then
+                Larg_cassa = 0.8
+                Larg_cassa1 = 1.2
+                Alt_cassa = Larg_N + 0.15
+                Costo = 20
+            End If
+
+            If Larg_cassa < 1.2 And Larg_cassa > 0.8 Then
+                Larg_cassa = 1.2
+                Alt_cassa = Larg_N + 0.15
+                Costo = 20
+            End If
+
+            If Larg_cassa < 1.4 And Larg_cassa > 1.2 Then
+                Larg_cassa = 1.4
+                Alt_cassa = Larg_N + 0.15
+                Costo = 50
+            End If
+
+            If Larg_cassa < 1.8 And Larg_cassa > 1.4 Then
+                Larg_cassa = 1.8
+                Alt_cassa = Larg_N + 0.15
+                Costo = 100
+            End If
+
+            If Larg_cassa < 2.2 And Larg_cassa > 1.8 Then
+                Larg_cassa = 2.2
+                Alt_cassa = Larg_N + 0.15
+                Costo = 120
+            End If
+        End If
+
+        '40 piedi dry box
+        Lato_2 = Larg_N + 0.15                      'Larghezza
+            Lato_1 = 2.24                               'Altezza porta
+            Lato_3 = Math.Round(Vol_Imb / (Lato_1), 1)  'Lunghezza
+
+            'High cube
+            If Lato_3 > 11.8 Then
+                Lato_1 = 2.6
+                Lato_3 = Math.Round(Vol_Imb / (Lato_1), 1)
+            End If
+
+            'High cube con 1 giro laterale
+            If Lato_3 > 11.8 Then
+                Lato_2 = Lato_2 + Alt_b + 0.15
+                Lato_1 = 2.6
+                Lato_3 = 11.8
+            End If
+
+            '40 piedi high cube
+            If Lato_3 > 11.8 Then
+                Lato_2 = Larg_N + Larg_b * 2 + 0.15
+                Lato_1 = 2.6
+                nstrati = Lato_1 / (Alt_b + 0.04)
+                n = (Math.Round(nstrati - 0.4, 0) * 2) - 2
+            End If
+
+            If Lato_3 > 11.8 Then
+                MessageBox.Show("Imballo speciale", "Avviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1)
+                Exit Sub
+            End If
+
+            'Output
+            Me.Alt_I.Text = Lato_1
+        Me.Larg_I.Text = Lato_2
+        Me.Lungh_I.Text = Lato_3
+        Me.Mat.Text = Materiale
+    End Sub
+
+End Class
